94a01265ab4cd3e5a81aadcf29ad22098d5efcf1
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-01-17 15:31:59 +1100

    add timestamps to tour model and testing

--- database/schemas/tour_schema.js
+++ database/schemas/tour_schema.js
@@ -1,26 +1,31 @@
 const { Schema } = require("mongoose");
 
-const TourSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const TourSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    duration: {
+      type: String,
+      required: true
+    }
   },
-  image: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  duration: {
-    type: String,
-    required: true
+  {
+    timestamps: {}
   }
-});
+);
 
 module.exports = TourSchema;
