5eef77896804860d0e9021f9f4399ec4d452b142
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-02-06 11:58:43 +1100

    test middleware

--- middleware/error_handler_middleware.js
+++ middleware/error_handler_middleware.js
@@ -2,6 +2,7 @@ const { isCelebrate } = require("celebrate");
 const { deleteImage } = require("./../services/aws_service");
 
 module.exports = function(err, req, res, next) {
+  console.log(err.message);
   if (err && err.name === "HTTPError") {
     return res.status(err.statusCode).send(err.message);
   }
